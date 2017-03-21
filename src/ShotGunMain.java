7d6be0dc9fea489753f2c942ccaa00334ef31bc7
DESKTOP-CS2AL2S\nicol <nicolas.baumann@gmx.ch>
2017-03-21 14:13:36 +0100

    stuff
    s

--- src/ShotGunMain.java
+++ src/ShotGunMain.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -11,6 +8,7 @@ import java.net.Socket;
 public class ShotGunMain {
     private BufferedReader in;
     private PrintWriter out;
+    private BufferedWriter bufferedWriter;
 
     public static void main(String[] args) throws IOException {
         ShotGunMain shotGunMain = new ShotGunMain();
@@ -18,23 +16,33 @@ public class ShotGunMain {
     }
 
     public void connectToServer() throws IOException {
-
         // Get the server address from a dialog box.
         String serverAddress = "virt14.ethz.ch";
-
+        String response = " ";
         // Make connection and initialize streams
         Socket socket = new Socket(serverAddress, 1500);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
 
-        // Consume the initial welcoming messages from the server
-        for (int i = 0; i < 3; i++) {
-            System.out.println(in.readLine());
+        response = read(socket);
+        if (response.startsWith("Hello")) {
+            System.out.println("starting authentication");
+            write(socket, "Auth bee2fl3");
+            System.out.println(read(socket));
         }
     }
 
-    public void authenticateToServer(){
-        
+
+    public String read(Socket socket) throws IOException {
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+        String response = in.readLine();
+        System.out.println(response);
+        return response;
+    }
+
+    public void write(Socket socket, String message) throws IOException {
+        bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        bufferedWriter.write(message);
+        System.out.println(message);
     }
 
 }
